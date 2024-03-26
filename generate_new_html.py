e400fc6db1da81b30855b27ce3ce377fcca1e200
Liufeng1923 <liufeng1923@gmail.com>
2024-03-26 17:28:30 +0900

    added something

--- generate_new_html.py
+++ generate_new_html.py
@@ -6,13 +6,20 @@ def get_all_files():
     all_files = os.listdir("MarkdownFiles")
     for file in all_files:
         if file.endswith(".md"):
-            print(file)
-            os.system(
-                "markmap --no-open --output ./html/{}.html ./MarkdownFiles/{}".format(
-                    file.split(".")[0], file
+            if file.startswith("index"):
+                os.system(
+                    "markmap --no-open --output index.html ./MarkdownFiles/{}".format(
+                        file
+                    )
                 )
-            )
-            print("{} coverted!".format(file))
+                print("{} coverted!".format(file))
+            else:
+                os.system(
+                    "markmap --no-open --output ./html/{}.html ./MarkdownFiles/{}".format(
+                        file.split(".")[0], file
+                    )
+                )
+                print("{} coverted!".format(file))
 
 
 if __name__ == "__main__":
